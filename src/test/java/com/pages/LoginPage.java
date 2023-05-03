9bcf5a44c630fbc934e5e346027d8a6b31d208de
ilhanTabak <herr.ilhantabak@gmail.com>
2023-05-03 14:03:23 +0200

    kedi

--- src/test/java/com/pages/LoginPage.java
+++ src/test/java/com/pages/LoginPage.java
@@ -1,6 +1,7 @@
 package com.pages;
 
 import com.utilities.ConfigurationReader;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -8,6 +9,12 @@ public  class LoginPage extends BasePage {
 
     @FindBy(xpath = "//a[text()='Log in']")
     public WebElement loginBtn;
+
+    @FindBy(className = "ico-logout")
+    public WebElement logOut;
+
+    @FindBy(className = "page-title")
+    public WebElement titleText;
 
     @FindBy(id = "Email")
     public WebElement emailBox;
@@ -18,13 +25,33 @@ public  class LoginPage extends BasePage {
     @FindBy(xpath = "//input[@class='button-1 login-button']")
     public WebElement loginSubmit;
 
+    @FindBy(className ="topic-html-content-header")
+    public WebElement textControl;
+
+    @FindBy(className = "validation-summary-errors")
+    public WebElement unsuccess;
+
+    @FindBy(xpath = "//span[@for='Email']")
+    public WebElement invaildMail;
+
     public void login(){
         loginBtn.click();
+        Assert.assertTrue(titleText.getText().contains("Sign In!"));
         emailBox.sendKeys(ConfigurationReader.get("loginMail"));
         passwordBox.sendKeys(ConfigurationReader.get("loginPassword"));
-   
         loginSubmit.click();
     }
+    public void invalidLogin(){
+        logOut.click();
+        loginBtn.click();
+        Assert.assertTrue(titleText.getText().contains("Sign In!"));
+        emailBox.sendKeys("ilhan@gmail.com");
+        passwordBox.sendKeys("12345");
+        loginSubmit.click();
+    }
+
+
+
 
 
 
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,6 +1,43 @@
 Feature: Demo Webshop log in Test
 
-  Scenario: Log in as a User
+  @wip
+  Scenario:As a User can log in with valid credentials
     Given The user is on the login page
     When The user enters valid credentials
     Then The user should be able to login
+
+  @wip
+  Scenario Outline:Login Negatif Test
+    Given The user is on the login page
+    When  The user enters "<userName>" and "<password>"
+    Then The user should be able not to login
+    And The user take a text contain"<message>"
+    Examples:
+      | userName        | password | message      |
+      | abc@gmail.com   | 1234     | unsuccessful |
+      |                 | sifreyok | unsuccessful |
+      | grup1@gmail.com |          | unsuccessful |
+      |                 |          | unsuccessful |
+
+
+@wip
+  Scenario Outline:Email Adresse Negatif Test
+    Given The user is on the login page
+    When  The user enters "<userName>" and "<password>"
+    Then The user should be able not to login
+    And The user must take a text contain"<message>"
+    Examples:
+      | userName        | password | message     |
+      | abcgmail.com    | sifreyok | a valid email |
+      | @abc.gmail.com  | sifreyok | a valid email |
+      | grup1gmail.com@ | sifreyok | a valid email |
+      | abc@gmail       | sifreyok | a valid email |
+      | abc@gmail@      | sifreyok | a valid email |
+
+
+
+
+
+
+
+
