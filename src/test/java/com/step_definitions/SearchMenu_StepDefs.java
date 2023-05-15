b961b98625859dc70577883ff1a0725bc70140f6
Bayram Bakir <bayrambakir46@gmail.com>
2023-05-15 10:43:21 +0200

    15.05

--- src/test/java/com/step_definitions/SearchMenu_StepDefs.java
+++ src/test/java/com/step_definitions/SearchMenu_StepDefs.java
@@ -1,46 +1,53 @@
 package com.step_definitions;
 
+import com.github.dockerjava.api.model.Image;
 import com.pages.LoginPage;
+import com.pages.SearchMenuPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.eo.Se;
+import org.junit.Assert;
 
 public class SearchMenu_StepDefs {
     LoginPage loginPage=new LoginPage();
+SearchMenuPage searchMenuPage=new SearchMenuPage();
+
+    @When("The user searches for the product on the website, they must verify that the {string} is displayed")
+    public void theUserSearchesForTheProductOnTheWebsiteTheyMustVerifyThatTheIsDisplayed(String text) {
+        searchMenuPage.searchMenu(text);
 
 
-    @When("The user searches for the product on the website, they must verify that the product is displayed")
-    public void theUserSearchesForTheProductOnTheWebsiteTheyMustVerifyThatTheProductIsDisplayed() {
     }
 
     @Then("Verify item not found is displayed when user searches for item not on website")
     public void verifyItemNotFoundIsDisplayedWhenUserSearchesForItemNotOnWebsite() {
-        
+        System.out.println("searchMenuPage.searchResult.getText() = " + searchMenuPage.searchResult.getText());
+        Assert.assertTrue(searchMenuPage.searchResult.getText().contains("No products"));
     }
 
-    @And("Can verify that search results contain accurate and up-to-date information on product features and pricing")
-    public void canVerifyThatSearchResultsContainAccurateAndUpToDateInformationOnProductFeaturesAndPricing() {
-        
+
+
+
+    @And("Search menu is case sensitive {string}")
+    public void searchMenuIsCaseSensitive(String text) {
+        searchMenuPage.searchMenu(text);
+
+    }
+    @And("search menu works with numbers and signs {string}")
+    public void searchMenuWorksWithNumbersAndSigns(String text) {
+        searchMenuPage.searchMenu(text);
     }
 
-    @When("Users can save search results according to the criteria they want")
-    public void usersCanSaveSearchResultsAccordingToTheCriteriaTheyWant() {
-        
-    }
 
-   
 
-    @And("Search menu is case sensitive")
-    public void searchMenuIsCaseSensitive() {
-    }
 
-    @And("search menu works with numbers and signs")
-    public void searchMenuWorksWithNumbersAndSigns() {
-        
-    }
+    @And("A photo or price of any product should appear on the search results page {string}")
+    public void aPhotoOrPriceOfAnyProductShouldAppearOnTheSearchResultsPage(String text) {
+        searchMenuPage.searchMenu(text);
 
-    @And("A photo or price of any product should appear on the search results page")
-    public void aPhotoOrPriceOfAnyProductShouldAppearOnTheSearchResultsPage() {
+
+        Assert.assertTrue(searchMenuPage.Image.getText().contains("Build your own cheap computer"));
 
     }
 }
--- src/test/resources/features/SearchMenu.feature
+++ src/test/resources/features/SearchMenu.feature
@@ -1,20 +1,20 @@
-@wip
 Feature: Login and search Test
 
-  Scenario  As a user I should be able to search in the search engine
-    Given The user is on the login page
+  @wip
+  Scenario:  As a user I should be able to search in the search engine
 
-    When  The user searches for the product on the website, they must verify that the product is displayed
+    Given The user is on the login page
+    When The user enters valid credentials
+    Then The user should be able to login
+    When  The user searches for the product on the website, they must verify that the "books" is displayed
     Then Verify item not found is displayed when user searches for item not on website
-    And Can verify that search results contain accurate and up-to-date information on product features and pricing
-    When Users can save search results according to the criteria they want
-    And Search menu is case sensitive
-    And search menu works with numbers and signs
-    And A photo or price of any product should appear on the search results page
+    And Search menu is case sensitive "BOOKS"
+    And search menu works with numbers and signs "*@1§4"
+    And A photo or price of any product should appear on the search results page "Computer"
 
 #kullanici urunu web sitesinde arama yaptiginda urunun göruntulendigini dogrulayabilmeli
 #kullanicinin web sitesinde olmayan ogeyi aradiginda "urun bulunamadi mesajini goruntulendigini dogrulayin"
-#Arama sonuçları, ürün özellikleri ve fiyatlandırma konusunda doğru ve güncel bilgiler içerdigini dogrulayabilmeli
+
 #Kullanıcılar, arama sonuçlarını istedikleri kriterlere göre kaydedebilir .
 #search menusu case sensitivdir
 #search menüsü rakam ve isaretlerle calisir
